78dc45daa032cf8a5d9f0fcca14dbc05b4804b78
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-25 13:33:04 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,7 +7,7 @@ class Project
   end
 
   def add_backer(backer)
-    self.backers << backer unless self.backer.include?(backer)
+    self.backers << backer unless self.backers.include?(backer)
     backer.back_project(self)
   end
 
